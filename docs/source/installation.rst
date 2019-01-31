13ef5f215ee1541e4d8d913a9c92da571c9e4d2b
Kyle Wilcox <kyle@axiomdatascience.com>
2019-01-31 11:16:23 -0500

    Improve CI and conda-recipe

--- docs/source/installation.rst
+++ docs/source/installation.rst
@@ -4,3 +4,11 @@ Installation
 At the command line::
 
     $ conda install -c axiom-data-science ioos_qc
+
+
+Development and Testing
+-----------------------
+
+    $ conda create -n ioosqc37 python=3.7
+    $ conda activate ioosqc37
+    $ conda install --file requirements.txt --file tests/requirements.txt
--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: GIS',
         'Topic :: Scientific/Engineering :: Information Analysis',
