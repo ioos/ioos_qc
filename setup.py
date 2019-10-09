772652bcf31533254621e775b3b4371a54e630f1
Jessica Austin <jessica@axiomalaska.com>
2019-10-09 08:52:50 -0400

    remove python 3.5 support

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ setup(
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Topic :: Scientific/Engineering',
