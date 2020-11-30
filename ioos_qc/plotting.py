0560e451e662f87955454eef6e4b6be1aae7a82a
Kyle Wilcox <kyle@axiomdatascience.com>
2020-11-30 12:58:23 -0500

    Add Bokeh plotting shortcuts for a list of CollectionResult objects

--- ioos_qc/plotting.py
+++ ioos_qc/plotting.py
@@ -65,3 +65,44 @@ def bokeh_multi_var(stream, results, title):
             for testname, testresults in tests.items():
                 plt = bokeh_plot_var(stream.time(), stream.data(vname), vname, qcobj, title, modu, testname)
                 yield plt
+
+
+def bokeh_plot_collected_results(results, **kwargs):
+    kwargs = {
+        **{
+            'merge_tools': True,
+            'toolbar_location': 'below',
+            'sizing_mode': 'scale_width',
+            'plot_width': 600,
+            'plot_height': 200,
+            'ncols': 2
+        },
+        **kwargs
+    }
+
+    plots = []
+    for r in results:
+        if r.data.any() and r.results.any():
+            plots.append(bokeh_plot_collected_result(r))
+    return gridplot(plots, **kwargs)
+
+
+def bokeh_plot_collected_result(cr):
+    title = f'{cr.stream_id}: {cr.package}.{cr.test}'
+    p1 = plotting.figure(x_axis_type="datetime", title=title)
+    p1.grid.grid_line_alpha = 0.3
+    p1.xaxis.axis_label = 'Time'
+    p1.yaxis.axis_label = 'Data'
+    qc_pass = np.ma.masked_where(cr.results != 1, cr.data)
+    qc_suspect = np.ma.masked_where(cr.results != 3, cr.data)
+    qc_fail = np.ma.masked_where(cr.results != 4, cr.data)
+    qc_notrun = np.ma.masked_where(cr.results != 2, cr.data)
+
+    p1.line(cr.tinp, cr.data,  legend_label='data', color='#A6CEE3')
+    p1.circle(cr.tinp, qc_notrun, size=3, legend_label='qc not run', color='gray', alpha=0.2)
+    p1.circle(cr.tinp, qc_pass, size=4, legend_label='qc pass', color='green', alpha=0.5)
+    p1.circle(cr.tinp, qc_suspect, size=4, legend_label='qc suspect', color='orange', alpha=0.7)
+    p1.circle(cr.tinp, qc_fail, size=6, legend_label='qc fail', color='red', alpha=1.0)
+    p1.circle(cr.tinp, qc_notrun, size=3, legend_label='qc not eval', color='gray', alpha=1.0)
+
+    return p1
