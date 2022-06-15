0c52319127bc2f399422b8b7671155a87bc3fd1e
Filipe Fernandes <ocefpaf@gmail.com>
2022-06-15 10:47:48 -0300

    use nbsphinx to fix examples and fix none language deprecation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,7 +42,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx_autodoc_typehints',
-    #"nbsphinx",
+    "nbsphinx",
 ]
 
 napoleon_google_docstring = True
@@ -93,7 +93,7 @@ release = "2.0.1"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
