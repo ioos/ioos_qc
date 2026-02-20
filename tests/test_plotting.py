1873c7ec9c2337b0c1d184651c53be8f616634a0
PrasannaPal21 <prasannapal273@gmail.com>
2026-02-20 10:40:27 +0530

    style: fix pre-commit issues in plotting tests

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -10,7 +10,8 @@ from ioos_qc.streams import NumpyStream, PandasStream
 class TestBokehPlotting(unittest.TestCase):
     def setUp(self):
         self.time = pd.date_range("2020-01-01", periods=10)
-        self.data = np.random.randn(10)
+        rng = np.random.default_rng()
+        self.data = rng.standard_normal(10)
         self.flags = np.ones(10) * 1
 
         self.results = {
@@ -22,7 +23,7 @@ class TestBokehPlotting(unittest.TestCase):
         }
 
     def test_bokeh_plot(self):
-        """Smoke test for single variable bokeh plot"""
+        """Smoke test for single variable bokeh plot."""
         plot = plotting.bokeh_plot(
             self.time,
             self.data,
@@ -35,14 +36,14 @@ class TestBokehPlotting(unittest.TestCase):
         assert plot is not None
 
     def test_bokeh_multi_var_pandas_stream(self):
-        """Smoke test for multi-variable bokeh plot with PandasStream"""
+        """Smoke test for multi-variable bokeh plot with PandasStream."""
         df = pd.DataFrame({"time": self.time, "temp": self.data})
         stream = PandasStream(df, time="time")
         plot = plotting.bokeh_multi_plot(stream, self.results, "Test Plot")
         assert plot is not None
 
     def test_bokeh_multi_var_numpy_stream(self):
-        """Smoke test for multi-variable bokeh plot with NumpyStream"""
+        """Smoke test for multi-variable bokeh plot with NumpyStream."""
         stream = NumpyStream(inp={"temp": self.data}, time=self.time)
         plot = plotting.bokeh_multi_plot(stream, self.results, "Test Plot")
         assert plot is not None
